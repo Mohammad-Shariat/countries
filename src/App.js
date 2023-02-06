3e31052274fa7c170c8a0e5ff7d0895c79001ae7
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2023-02-06 22:01:15 +0330

    get data from server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+
+//* API
+import { getData } from './services/api';
+
 function App() {
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await getData();
+      console.log(data);
+    };
+
+    fetchAPI();
+  }, []);
+
   return (
     <div className='App'>
       <h1>Countries</h1>
