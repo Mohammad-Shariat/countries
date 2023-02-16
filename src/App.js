4ecc0c9a33b6c307bbf9a56ae8e6b2fdc1c8bb05
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2023-02-16 19:57:23 +0330

    insert react-router-dom and countries to app

--- src/App.js
+++ src/App.js
@@ -1,68 +1,15 @@
-import { useState, useEffect } from 'react';
-import { Card4 } from './components/Card';
-import SearchInput from './components/SearchInput';
-import DropdownCountry from './components/Dropdown';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Countries from './components/Countries';
 
-// NextUi
-import { NextUIProvider, Grid, Text } from '@nextui-org/react';
-
-//URL
-const BASE_URL = 'https://restcountries.com/v3.1/all';
-
-function App() {
-  const [countriesData, setCountriesData] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then(res => res.json())
-      .then(data => setCountriesData(data));
-  }, []);
-
-  const searchHandler = event => {
-    setSearch(event.target.value);
-  };
-
-  const searchCountries = countriesData.filter(country =>
-    country.name.common.toLowerCase().includes(search.toLocaleLowerCase())
-  );
-
-  console.log(countriesData);
-
+const App = () => {
   return (
-    <NextUIProvider>
-      <Grid.Container gap={2}>
-        <Grid>
-          <Text color='#555' h2>
-            Where in the world ?
-          </Text>
-        </Grid>
-      </Grid.Container>
-
-      <Grid.Container gap={2} justify='space-between' alignItems='center'>
-        <Grid>
-          <SearchInput
-            countriesData={countriesData}
-            value={search}
-            onChange={searchHandler}
-          />
-        </Grid>
-        <Grid>
-          <DropdownCountry />
-        </Grid>
-      </Grid.Container>
-
-      <Grid.Container gap={2} justify='center'>
-        {searchCountries?.map((country, index) => {
-          return (
-            <Grid xl={2} md={4} sm={6} xs={12} key={index}>
-              <Card4 data={country} />
-            </Grid>
-          );
-        })}
-      </Grid.Container>
-    </NextUIProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Countries />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
